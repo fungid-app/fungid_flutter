9fee6a28a377dc6d4b720392854714c4c2f32098
Michael Weishuhn <weishuhn@gmail.com>
2024-09-30 12:35:33 -0600

    Updated based on new API version

--- lib/presentation/widgets/add_image_sheet.dart
+++ lib/presentation/widgets/add_image_sheet.dart
@@ -19,8 +19,8 @@ void createAddImageSheet({
         images.add(image);
       }
     } else {
-      List<XFile> imageList = await imagePicker.pickMultiImage();
-      if (imageList.isNotEmpty) {
+      List<XFile>? imageList = await imagePicker.pickMultiImage();
+      if (imageList != null && imageList.isNotEmpty) {
         images.addAll(imageList);
       }
     }
